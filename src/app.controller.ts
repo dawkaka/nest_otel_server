1eced5fca22dbbb19948230a685ab9d29825ab30
dawkaka <yousiph77@gmail.com>
2024-08-26 23:08:02 +0000

    clean up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AxiosService } from './axios.service';
 
 @Controller()
 export class AppController {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AxiosService } from './axios.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, AxiosService],
+  providers: [AppService],
 })
 export class AppModule {}
