26e92454fad36e7d447c89f405f855b1381120b3
Anthony Alaribe <anthonyalaribe@gmail.com>
2024-08-31 15:23:24 +0200

    Update telemetry.ts

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -9,16 +9,6 @@ import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
-// const defaultAttributes = {
-//   [SemanticResourceAttributes.SERVICE_NAME]: 'apitoolkit.server',
-//   [SemanticResourceAttributes.SERVICE_VERSION]: '1.0.0',
-//   environment: 'production',
-//   'at-project-id': '00000000-0000-0000-0000-000000000000',
-//   'at-project-key': 'kKMdJZdMPikzn91K0qZsTzsc9DjBSYCe6bjp0b9fojtT9Y3C'
-// };
-// 
-// const resource = new Resource(defaultAttributes);
-
 const logExporter = new OTLPLogExporter({
   url: 'http://otelcol.apitoolkit.io:4317', //grpc endpoint
 });
